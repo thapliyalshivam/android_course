b20dcd6bbc04fccdfe76f898214d783bb78dff71
Shivam Thapliyal <thapliyalshivam@gmail.com>
2018-12-23 00:50:47 +0530

    resolved

---
+++ app/src/main/java/com/example/tshivam/commun/Response.kt
@@ -0,0 +1,14 @@
+package com.example.tshivam.commun
+
+import com.google.gson.annotations.SerializedName
+data class Response(
+
+	@field:SerializedName("total_count")
+	val totalCount: Int? = null,
+
+	@field:SerializedName("incomplete_results")
+	val incompleteResults: Boolean? = null,
+
+	@field:SerializedName("items")
+	val items: List<ItemsItem?>? = null
+)
